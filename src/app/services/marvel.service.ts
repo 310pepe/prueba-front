f385b43d9e52389c73b3cb94334e022b73941893
310pepe <joseber310@gmail.com>
2021-08-10 07:56:57 -0500

    primer home

--- src/app/services/marvel.service.ts
+++ src/app/services/marvel.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class MarvelService {
 
-  constructor() { }
+  url='http://gateway.marvel.com/v1/public/characters?apikey=e10449344c3ed4d23dbc39c2bf657aa8&hash=ac9cd13f55ac10371645a0126b7f009c&ts=abcdefghijk';
+
+  constructor( private http:HttpClient ) { }
+
+  
+
+  getPersonajes()
+  {
+    return this.http.get(`${this.url}`);
+  }
+
 }
